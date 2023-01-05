14e4d6ffa76f112c6d66ffb483d920574ab7488f
Marcus Martins <vini.mark2002@gmail.com>
2023-01-04 21:49:31 -0300

    Fim Lista 9

--- Lista 9/D - Busca geral num conjunto não ordenado/busca-geral.c
+++ Lista 9/D - Busca geral num conjunto não ordenado/busca-geral.c
@@ -1,38 +1,92 @@
-/* #define key(A) (A)
-#define less(A, B) (key(A) < key(B))
-#define exch(A, B) { Item t = A; A = B; B = t; }
-#define cmpexch(A, B) { if (less(B, A)) exch(A, B); }
+#define key(A) (A)
+#define less(A, B) ((A) < (B))
+#define exch(A, B) { typeof(A) t = A; A = B; B = t; }
+#define cmpexch(A, B) { if (less(A, B)) exch(A, B); }
 
-typedef long int Item;
+typedef struct Item
+{
+    int numero;
+    int indice;
+} Item;
 
 #include <stdio.h>
+#include <stdlib.h>
 
-long busca_geral(Item *vetor, Item numero, unsigned long pos, unsigned long right)
-{
-    for (pos = 0; pos < right; pos++)
-        if ( vetor[pos] == numero )
-            return pos;
-    
-    return -1;
-}
+void insertionsort(Item *vetor, int left, int right);
+int busca_binaria(Item *vetor, int temp, int left, int right);
 
 int main()
 {
-    long m, n;
+    int m, n, temp;
 
-    scanf("%ld %ld", &n, &m);
+    scanf("%d %d", &n, &m);
 
     Item vetor[n];
-    Item temp;
 
     for (int i = 0; i < n; i++)
-        scanf("%lu", &vetor[i]);
+    {
+        scanf("%d", &vetor[i].numero);
+        vetor[i].indice = i;
+    }
+    
+    insertionsort(vetor, 0, n);
 
     for (int i = 0; i < m; i++)
     {
-        scanf("%ld", &temp);
-        printf( "%ld\n", busca_geral(vetor, temp, 0, n) );
+        scanf("%d", &temp);
+        int *res = malloc(sizeof(int)*4);
+        res = busca_binaria(vetor, temp, 0, n);
+        printf("%d\n", res);
     }
 
  return 0;
-} */
+}
+
+void insertionsort(Item *vetor, int left, int right)
+{
+    for (int i = right; i > left; i--)
+    {
+        if ( less(vetor[i-1].numero, vetor[i].numero) )
+            exch(vetor[i-1], vetor[i]);
+    }
+
+
+    for (int i = left + 2; i <= right; i++)
+    {
+        int j = i;
+        Item temp = vetor[j];
+
+        while ( less(temp.numero, vetor[j - 1].numero) )
+        {
+            vetor[j] = vetor[j-1];
+            j--;
+        }
+        
+        vetor[j] = temp;
+
+        printf("\n");
+        for (int k = 0; k < right; k++)
+        {
+            printf("%d\n", vetor[k].numero);
+        }
+        printf("\n");
+    }
+}
+
+int busca_binaria(Item *vetor, int temp, int left, int right)
+{
+    if (left > right)
+        return -1;
+
+    int meio = (right+left)/2;
+    Item elemento_meio = vetor[meio];
+    
+    if (less(temp, elemento_meio.numero))
+        return busca_binaria(vetor, temp, left, meio);
+    else
+        if (less(elemento_meio.numero, temp))
+            return busca_binaria(vetor, temp, meio+1, right);
+    
+    if ( temp == elemento_meio.numero )
+        return elemento_meio.indice;
+}
